6ee81370f5040deeb83b50c1f6e176e996fcd922
Kinjal Kamdar <kkamdar@GIN-DESKTOP-003.GANTNERGROUP.LOCAL>
2020-02-10 17:01:38 +0530

    first commit

---
+++ StackService.Tests/StackTests.cs
@@ -0,0 +1,51 @@
+using NUnit.Framework;
+using StackService.Services;
+
+namespace Tests.UnitTests.Services
+{
+    [TestFixture]
+    public class StackTests
+    {
+        //StackS _stack;
+        [SetUp]
+        public void Setup()
+        {
+            //_stack = new StackS();
+        }
+
+        [Test]
+        public void Creation()
+        {
+            StackS<int> s = new StackS<int>(3);
+            Assert.AreEqual(0,s.Size);
+        }
+        StackS<int> s = new StackS<int>(3);
+        /*[Test]
+        public void PushOp()
+        {
+            StackS<int> s = new StackS<int>(3);
+            s.Push(1);
+            s.Push(2);
+            s.Push(3);
+            Assert.AreEqual(3,s.Size);
+        }
+        [Test]
+        public void PopOp()
+        {
+            StackS<int> s = new StackS<int>(3);
+            s.Push(1);
+            int value= s.Pop();
+            Assert.AreEqual(2, value);
+        }*/
+        [Test]
+        public void Push_Pop()
+        {
+            s.Push(1);
+            s.Push(2);
+            s.Push(3);
+            int value =s.Pop();
+            Assert.AreEqual(3,value);
+            Assert.AreEqual(2,s.Size);
+        }
+    }
+}
---
+++ StackService/StackS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StackService.Services
+{
+    public class StackS<T>
+    {
+        private T[] stackArray;
+        private int maximumLength;
+        public int Size {get; set;}
+        public StackS(int length)
+        {
+            maximumLength = length;
+            stackArray = new T[length];
+        }
+        public void Push(T value)
+        {
+            stackArray[Size++] = value;
+            //return value;
+        }
+
+        public T Pop()
+        {
+            return stackArray[--Size];
+        }
+    }
+}
