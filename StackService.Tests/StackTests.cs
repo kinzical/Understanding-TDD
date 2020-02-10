1d95fa326150e3aac6b557a41314f79bee4548dc
Kinjal Kamdar <kkamdar@GIN-DESKTOP-003.GANTNERGROUP.LOCAL>
2020-02-10 18:43:18 +0530

    executed few test cases

--- StackService.Tests/StackTests.cs
+++ StackService.Tests/StackTests.cs
@@ -20,7 +20,7 @@ namespace Tests.UnitTests.Services
             StackS<int> s = new StackS<int>(3);
             Assert.AreEqual(0,s.Size);
         }
-        StackS<int> s = new StackS<int>(3);
+        //StackS<int> s = new StackS<int>(3);
         /*[Test]
         public void PushOp()
         {
@@ -41,6 +41,7 @@ namespace Tests.UnitTests.Services
         [Test]
         public void Push_Pop()
         {
+            StackS<int> s = new StackS<int>(3);
             s.Push(1);
             s.Push(2);
             s.Push(3);
@@ -48,5 +49,18 @@ namespace Tests.UnitTests.Services
             Assert.AreEqual(3,value);
             Assert.AreEqual(2,s.Size);
         }
+        [Test]
+        public void PopException()
+        {
+            StackS<int> s = new StackS<int>(3);
+            Assert.Throws<ProhibitedPop>(() => s.Pop());
+        }
+        [Test]
+        public void PushException()
+        {
+            StackS<int> s = new StackS<int>(3);
+            Assert.Throws<ProhibitedPush>(() => s.Pop());
+        }
+        
     }
 }
